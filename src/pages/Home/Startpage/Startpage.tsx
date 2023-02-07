bd25fa107509e01c12152f4713c7630fbf229e58
jacsonikowy <michal6343@wp.pl>
2023-02-07 13:48:30 +0100

    fix: css changes and structure of tsx

--- src/pages/Home/Startpage/Startpage.tsx
+++ src/pages/Home/Startpage/Startpage.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import TextBlock from '../../../components/TextBlock'
+import Button from '../../../components/Button/Button'
+import './Startpage.scss'
 
 const Startpage: React.FC = () => {
     return (
-        <div>
-            <TextBlock headingText='Building stellar websites for early startups' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'/>
+        <div className='startpage'>
+            <div className="container">
+                <TextBlock headingText='Building stellar websites for early startups' level="h1" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'/>
+                <span>
+                    <Button text='View our work' />
+                </span>
+            </div>
         </div>
     )
 }
