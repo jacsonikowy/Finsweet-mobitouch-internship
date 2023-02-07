54580575f9ba81bdd59a6bbbf4508e892cb2fce7
jacsonikowy <michal6343@wp.pl>
2023-02-07 14:15:48 +0100

    fix: add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
 
 
 function App() {
   return (
     <>
-    <Navbar />
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit excepturi voluptatibus possimus, iste maxime nisi assumenda atque cumque, ab quo labore error repellat quod fugiat quos obcaecati optio? Doloremque, dicta.
-    <Footer />
+    <Home />
     </>
   );
 }
