4d2c64e8a0ce40be8bf32d9e3cab3398ac38bbee
jacsonikowy <michal6343@wp.pl>
2023-02-07 14:58:41 +0100

    fix: add padding

--- src/pages/Home/Projects/Projects.tsx
+++ src/pages/Home/Projects/Projects.tsx
@@ -5,7 +5,7 @@ import './Projects.scss'
 const Projects: React.FC = () => {
     return (
         <div className='projects'>
-            
+            <h1>View our projects</h1>
         </div>
     )
 }
