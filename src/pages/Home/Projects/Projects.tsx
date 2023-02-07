f7469a2429ca49a22590a79ee26ab059aabebde7
jacsonikowy <michal6343@wp.pl>
2023-02-07 13:42:51 +0100

    feat: add projects

---
+++ src/pages/Home/Projects/Projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Projects.scss'
+
+const Projects: React.FC = () => {
+    return (
+        <div className='projects'>
+            
+        </div>
+    )
+}
+
+export default Projects
