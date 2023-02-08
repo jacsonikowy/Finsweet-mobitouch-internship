f81a737035955ed5b2696bbfdd7be7298bef08e7
jacsonikowy <michal6343@wp.pl>
2023-02-08 13:20:43 +0100

    fix(projects): add few images and changes in css

--- src/pages/Home/Projects/Projects.tsx
+++ src/pages/Home/Projects/Projects.tsx
@@ -2,10 +2,24 @@ import React from 'react'
 
 import './Projects.scss'
 
+import Workhub from '../../../assets/images/Project1.png';
+import Unisaas from '../../../assets/images/Unisaas.png';
+import Church from '../../../assets/images/church.png';
+
 const Projects: React.FC = () => {
     return (
         <div className='projects'>
             <h1>View our projects</h1>
+            <div className='projectsGrid'>
+                <div>
+                    <span className='rectangle'></span>
+                    <img src={Workhub} alt="" />
+                </div>
+                <div className="twoImages">
+                    <img src={Unisaas} alt="" />
+                    <img src={Church} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
