ea219eca6a47850d8c2df464756ea58c1d2f43be
jacsonikowy <michal6343@wp.pl>
2023-02-08 14:46:55 +0100

    fix: delete useless divs

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,15 @@
-import Button from '../Button/Button'
+import Button from '../../components/Button/Button'
 import Links from './Links'
 import './Navbar.scss'
 
 const Navbar = () => {
     return (
         <nav>
-            <div>
-                <h6>{'{Finsweet'}</h6>
-            </div>
+            <h6>{'{Finsweet'}</h6>
             <div className='navlinks'>
                 <Links />
-                <div>
-                    <Button text="Contact Us" />
-                </div>
+                <Button text="Contact Us" background='#1C1E53' textColor='white' />
             </div>
-            
         </nav>
     )
 }
