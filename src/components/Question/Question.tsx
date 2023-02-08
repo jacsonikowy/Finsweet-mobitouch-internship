2a1caf31ae558c6c9da2a5e8da74a8b14865d921
jacsonikowy <michal6343@wp.pl>
2023-02-08 14:44:54 +0100

    feat: add new component question

---
+++ src/components/Question/Question.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './Question.scss';
+
+import Plus from '../../assets/icons/plus.png';
+
+
+interface QuestionProps extends React.HTMLAttributes<HTMLButtonElement> {
+    number: string,
+    question: string,
+    answer: string,
+}
+
+
+const Question: React.FC<QuestionProps> = ({number,question,answer, ...props}) => {
+
+    const [active, setActive] = React.useState<boolean>(false);
+
+    const showAnswer = () => {
+        setActive(!active)
+        console.log('test')
+    }
+    
+
+    return (
+        <div className='question'>
+            <div className='answer'>
+                <h6 className='number'>{number}</h6>
+                <h6>{question}</h6>
+                <p>{active ? answer : null }</p>
+            </div>
+            <img src={Plus} alt="" onClick={() => showAnswer()}/>
+        </div>
+    )
+}
+
+export default Question
