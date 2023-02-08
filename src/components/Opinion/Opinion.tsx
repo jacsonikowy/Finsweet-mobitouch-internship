256eaf9e0b48ff7bd13bacc3d7d2530aa29b1ca2
jacsonikowy <michal6343@wp.pl>
2023-02-08 14:45:34 +0100

    feat: add new component opinion

---
+++ src/components/Opinion/Opinion.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import './Opinion.scss'
+
+import LeftArrow from '../../assets/icons/LeftArrow.png'
+import RightArrow from '../../assets/icons/RightArrow.png'
+
+type opinionProps = {
+    avatar: string,
+    text: string,
+    fullname: string,
+    title: string,
+    className?: string,
+}
+
+const Opinion: React.FC<opinionProps> = ({avatar, text, fullname, title, className}) => {
+    return (
+        <div className='review'>
+            <h2>"{text}"</h2>
+            <div className='description'>
+                <img src={avatar} alt="" />
+                <div className='opinion'>
+                    <span>{fullname}</span>
+                    <p>{title}</p>
+                </div>
+            </div>
+            <div className='rectangles'>
+                <span>
+                    <img className='whiteRectangle rectangle' src={LeftArrow} alt="" />
+                </span>
+                <span>
+                    <img className='purpleRectangle rectangle' src={RightArrow} alt="" />
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Opinion
