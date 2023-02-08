69678b299f6f2ca24b276740c35f44fd5fd7d04a
jacsonikowy <michal6343@wp.pl>
2023-02-08 13:21:36 +0100

    fix(startpage): add ilustration

--- src/pages/Home/Startpage/Startpage.tsx
+++ src/pages/Home/Startpage/Startpage.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import TextBlock from '../../../components/TextBlock'
+import TextBlock from '../../../components/TextBlock/TextBlock'
 import Button from '../../../components/Button/Button'
 import './Startpage.scss'
+
+import Illustration from '../../../assets/images/Illustration.png';
 
 const Startpage: React.FC = () => {
     return (
         <div className='startpage'>
-            <div className="container">
-                <TextBlock headingText='Building stellar websites for early startups' level="h1" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'/>
-                <span>
-                    <Button text='View our work' />
-                </span>
+            <div className='textblock'>
+                <TextBlock style={{width: 562}} headingText='Building stellar websites for early startups' level="h1" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'/>
+                <img src={Illustration} alt="" />
+            </div>
+            <div>
+                <Button text='View our work' background='#FCD980' textColor='#1B1C2B' />
             </div>
         </div>
     )
