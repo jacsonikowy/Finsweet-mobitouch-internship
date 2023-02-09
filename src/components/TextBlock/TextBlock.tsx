77dbc8145135294abab2e63edee6a5e81e88d3b8
jacsonikowy <michal6343@wp.pl>
2023-02-09 11:23:16 +0100

    chore: lowercase to uppercase name of prop

--- src/components/TextBlock/TextBlock.tsx
+++ src/components/TextBlock/TextBlock.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { createElement } from 'react'
 
+import Paragraph from './Paragraph/Paragraph'
+
 import './TextBlock.scss'
 
 type variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
@@ -8,23 +10,21 @@ type variant = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 export type textblockProps = {
     headingText: string,
     description: string,
-    level: string,
+    level: variant,
     className?: string,
-    style?: object,
 }
 
 const TextBlock: React.FC<textblockProps> = ({
     level,
     headingText,
     description,
-    style,
     className
 }) => {
     return createElement(
         level,
-        { style: {style}, className: `heading ${className}`},
+        {className: `heading ${className}`},
         headingText,
-        createElement('p', { className: 'description' }, description)
+        <Paragraph description={description}/>
     )
 }
 
