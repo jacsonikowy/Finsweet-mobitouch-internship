6565f816f2a9220b3686fb1529386eb1583a5dc4
jacsonikowy <michal6343@wp.pl>
2023-02-09 14:59:33 +0100

    fix: add ButtonArrow

--- src/pages/Home/Startpage/Startpage.tsx
+++ src/pages/Home/Startpage/Startpage.tsx
@@ -3,17 +3,19 @@ import TextBlock from '../../../components/TextBlock/TextBlock'
 import Button from '../../../components/Button/Button'
 import './Startpage.scss'
 
+import ButtonArrow from '../../../components/ButtonArrow/ButtonArrow'
 import Illustration from '../../../assets/images/Illustration.png';
 
 const Startpage: React.FC = () => {
     return (
         <div className='startpage'>
             <div className='textblock'>
-                <TextBlock style={{width: 562}} headingText='Building stellar websites for early startups' level="h1" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'/>
+                <TextBlock headingText='Building stellar websites for early startups' level="h1" description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.'/>
                 <img src={Illustration} alt="" />
             </div>
-            <div>
+            <div className='buttons'>
                 <Button text='View our work' background='#FCD980' textColor='#1B1C2B' />
+                <ButtonArrow text="View Pricing" color="white" />
             </div>
         </div>
     )
