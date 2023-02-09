4d8230acec60df471d8658b808d0fad60128718c
jacsonikowy <michal6343@wp.pl>
2023-02-09 15:05:25 +0100

    fix: create entire form

--- src/pages/Home/StellarForm/StellarForm.tsx
+++ src/pages/Home/StellarForm/StellarForm.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import { useFormik } from 'formik'
 
-import Convert from '../../../assets/images/womanwritingonpaper.png';
-import Background from '../../../assets/images/Background.png';
 
 import './StellarForm.scss'
 import TextBlock from '../../../components/TextBlock/TextBlock';
+import Button from '../../../components/Button/Button';
 
 const StellarForm: React.FC = () => {
 
-    const formik = useFormik({
+    /* const formik = useFormik({
         initialValues: {
             name: '',
             email: '',
@@ -19,15 +18,24 @@ const StellarForm: React.FC = () => {
             alert(JSON.stringify(values, null, 2));
         }
     })
+    */
 
     return (
-        <div className='stellar'>
-            <div className='backgroundImage'>
-                    <img src={Convert} alt="" />
-                <div className='stellarStart'>
-                    <TextBlock level='h2' headingText='Building stellar websites for early startups' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.' />
-                </div>
-                <img src={Background} alt="" />
+        <div className='grid'>
+            <div className='background'></div>
+            <div className='grid-item'>
+                <div className="background-1"></div>
+                <TextBlock className="text" level='h1' headingText='Building stellar websites for early startups' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim.' />
+            </div>
+            <div className='grid-item'>
+                <div className='background-2'></div>
+                <TextBlock className='text' level='h5' headingText='Send inquiry' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.' />
+                <form action="">
+                    <input className="body1" type="text" placeholder='Your Name'/>
+                    <input className="body1" type="text" placeholder='Email'/>
+                    <input className="body1" type="text" placeholder='Paste your Figma design URL'/>
+                    <Button className="submitBtn" text="Submit an Inquiry" background='#FCD980' textColor='black' />
+                </form>
             </div>
         </div>
     )
