c0de8b92049460595e84388a2d73e70c6aa7de93
jacsonikowy <michal6343@wp.pl>
2023-02-10 14:47:15 +0100

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import Home from './pages/Home/Home'
+import AboutUs from './pages/AboutUs/AboutUs'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/aboutus',
+    element: <AboutUs />
+  },
+  {
+
+  }
+])
 
 function App() {
   return (
     <>
-    <Home />
+      <RouterProvider router={router} />
     </>
   );
 }
