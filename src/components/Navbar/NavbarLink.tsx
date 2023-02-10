c74bd4076041ff59b85377f7dedd256019e731fe
jacsonikowy <michal6343@wp.pl>
2023-02-10 14:50:06 +0100

    fix: add function to change to lower character routes

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -1,8 +1,23 @@
 import './Navbar.scss';
 
+import { Link } from 'react-router-dom';
+
+interface Iformat {
+    (subpage: string): string
+}
+
+const getLinks: Iformat = (subpage: string) => {
+    if(subpage.toLowerCase() === "home") {
+        return '/'
+    }
+    return subpage.toLowerCase().split(" ").join("")
+} 
+
 const NavbarLink = ({subpage}: {subpage: string}) => {
     return (
-        <a className="navlink" href="#index">{subpage}</a>
+        <Link to={getLinks(subpage)}>
+            <a className="navlink" href="#index">{subpage}</a>
+        </Link>
     )
 }
 
