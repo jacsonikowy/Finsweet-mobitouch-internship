64a572d57e1d0fba37b65860ce5faa58907260ce
jacsonikowy <michal6345@wp.pl>
2023-02-12 17:27:38 +0100

    fix(footer): responsivness

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,36 +1,43 @@
-import React from 'react'
-import Links from "../Navbar/Links"
-import TextBlock from "../TextBlock/TextBlock"
-import './Footer.scss'
-
+import React from "react";
+import Links from "../Navbar/Links";
+import TextBlock from "../TextBlock/TextBlock";
+import "./Footer.scss";
 
 const Footer: React.FC = () => {
     return (
         <footer>
             <div className="footerEntry">
                 <div className="finsweet">
-                    <TextBlock headingText='{Finsweet' level='h3' description='We are always open to discuss your project and improve your online presence' />
-                    <div className='box'>
-                            <div>
-                                <p className='body2'>Email me at</p>
-                                <p className='body1 opacity'>contact@website.com</p>
-                            </div>
-                            <div>
-                                <p className='body2'>Call us</p>
-                                <p className='body1 opacity'>0927 6277 28525</p>
-                            </div>
+                    <TextBlock
+                        headingText="{Finsweet"
+                        level="h3"
+                        description="We are always open to discuss your project and improve your online presence"
+                    />
+                    <div className="box">
+                        <div>
+                            <p className="body2 box-title">Email me at</p>
+                            <p className="body1 opacity">contact@website.com</p>
+                        </div>
+                        <div>
+                            <p className="body2 box-title">Call us</p>
+                            <p className="body1 opacity">0927 6277 28525</p>
+                        </div>
                     </div>
                 </div>
-                <div className='letstalk'>
-                    <TextBlock headingText="Lets Talk!" level='h2' description='We are always open to discuss your project, improve your online presence and help with your UX/UI design challenges.' />
-                    </div>
+                <div className="letstalk">
+                    <TextBlock
+                        headingText="Lets Talk!"
+                        level="h2"
+                        description="We are always open to discuss your project, improve your online presence and help with your UX/UI design challenges."
+                    />
+                </div>
             </div>
-            <div className='footerEnd'>
+            <div className="footerEnd">
+                <Links className="footerEnd-links" />
                 <p>Copyright 2021, Finsweet.com</p>
-                <Links />
             </div>
         </footer>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
