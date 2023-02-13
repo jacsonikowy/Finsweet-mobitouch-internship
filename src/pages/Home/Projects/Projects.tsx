18cb77321f82906113ecf57ebef6fdf1f951b92e
jacsonikowy <michal6345@wp.pl>
2023-02-13 22:01:35 +0100

    fix(Projects): add some responsivness

--- src/pages/Home/Projects/Projects.tsx
+++ src/pages/Home/Projects/Projects.tsx
@@ -1,42 +1,40 @@
-import React from 'react'
+import React from "react";
 
-import './Projects.scss'
+import "./Projects.scss";
 
-import Workhub from '../../../assets/images/Project1.png';
-import Unisaas from '../../../assets/images/Unisaas.png';
-import Church from '../../../assets/images/church.png';
-import TextBlock from '../../../components/TextBlock/TextBlock';
-import ButtonArrow from '../../../components/ButtonArrow/ButtonArrow';
+import Workhub from "../../../assets/images/Project1.png";
+import Unisaas from "../../../assets/images/Unisaas.png";
+import Church from "../../../assets/images/church.png";
+import TextBlock from "../../../components/TextBlock/TextBlock";
+import ButtonArrow from "../../../components/ButtonArrow/ButtonArrow";
 
 const Projects: React.FC = () => {
     return (
         <div className="projects">
-            <h1>Our Projects</h1>
-            <div className="grid1">
-                <div className="grid-item1">
-                    <span className='rectangle'></span>
-                    <div className='workhub'>
-                        <TextBlock className='workhubtext' level='h5' headingText='Workhub office Webflow Webflow Design' description='Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam ' />
-                        <ButtonArrow className='buttonarrow' color='#FCD980' text="View project" />
-                    </div>
-                    <img src={Workhub} alt="" />
-                </div>
-                <div className='flex-items'>
-                    <div className="flex-item">
-                        <span className='rectangle-1'></span>
-                        <div className="unisaas">
-                            <h6>Unisaas Website Design</h6>
-                            <ButtonArrow className='buttonarrow' color='#FCD980' text='View project'/>
-                        </div>
-                        <img src={Unisaas} alt="" />
-                    </div>
-                    <div className="">
-                        <img src={Church} alt="" />
-                    </div>
+            <div className="item item-1">
+                <img src={Workhub} alt="workhub" />
+                <div className="item-block">
+                    <TextBlock
+                        className="item-block-textblock"
+                        level="h6"
+                        headingText="Worhub office Webflow Webflow Design"
+                        description="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam"
+                    />
+                    <ButtonArrow color="#FCD980" text="View project" />
                 </div>
             </div>
+            <div className="item item-2">
+                <div className="unisaas">
+                    <img src={Unisaas} alt="unisaas" />
+                    <div className="item-block2">
+                        <h6>Unisaas Wesbite Design</h6>
+                        <ButtonArrow color="#FCD980" text="View Portfolio" />
+                    </div>
+                </div>
+                <img src={Church} alt="church" />
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Projects
+export default Projects;
