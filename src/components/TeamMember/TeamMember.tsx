e921ccabb430674b0d1c1fe7e1fc387bceccd0ed
jacsonikowy <michal6343@wp.pl>
2023-02-13 14:45:11 +0100

    feat: TeamMember component

---
+++ src/components/TeamMember/TeamMember.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './TeamMember.scss'
+
+export interface TeamMemberProps {
+    img: string,
+    name: string,
+    title: string,
+}
+
+const TeamMember: React.FC<TeamMemberProps> = ({img, name, title}) => {
+    return (
+        <div className="teammember">
+            <img src={img} alt="Team Member" />
+            <h6>{name}</h6>
+            <p className='body1'>{title}</p>
+        </div>
+    )
+}
+
+export default TeamMember
