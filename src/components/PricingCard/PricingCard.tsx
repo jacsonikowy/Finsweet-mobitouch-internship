2051f28da3dab425b0395f99e2da665752bdb717
jacsonikowy <michal6343@wp.pl>
2023-02-13 14:44:46 +0100

    feat: pricingcard component

---
+++ src/components/PricingCard/PricingCard.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './PricingCard.scss'
+
+import TextBlock from '../../components/TextBlock/TextBlock'
+import Button from '../../components/Button/Button'
+
+import ActivePointer from '../../assets/icons/activePointer.svg'
+import UnactivePointer from '../../assets/icons/unactivePointer.svg'
+
+export interface LinksProps {
+    name: string,
+    active?: boolean
+}
+
+export type ArrayOfLinks = Array<LinksProps>
+
+export interface PricingCardProps {
+    price: string,
+    content: string,
+    backgroundColor: string,
+    textColor?: string,
+    productName: string,
+    productDescription: string,
+    ArrayOfLinks: ArrayOfLinks,
+    buttonName: string,
+    buttonTextColor: "white" | "black",
+    buttonBackgroundColor: "yellow" | "darkblue",
+}
+
+
+const PricingCard: React.FC<PricingCardProps> = ({price, content, backgroundColor, textColor, productName, productDescription, ArrayOfLinks, buttonName, buttonBackgroundColor, buttonTextColor}) => {
+    return (
+        <div style={{backgroundColor: backgroundColor, color: textColor}} className="pricingcard">
+            <div className="pricingcard-title">
+                <h3>{price}</h3>
+                <p className='body1'>{content}</p>
+            </div>
+            <TextBlock className="" level='h6' headingText={productName} description={productDescription} />
+            <div className='pricingcard-links'>
+            {ArrayOfLinks.map((link: LinksProps) => {
+                return (
+                    <span>
+                        {
+                            link.active ? <><img src={ActivePointer} alt="active pointer"/><p>{link.name}</p></> : <><img className='unactive' src={UnactivePointer} alt="unactive pointer"/><p>{link.name}</p></>
+                        }
+                    </span>
+                )
+            })}
+            </div>
+            <div className="pricingcard-buttondiv">
+                <Button text={buttonName} background={buttonBackgroundColor} textColor={buttonTextColor}/>
+            </div>
+        </div>
+    )
+}
+
+export default PricingCard
