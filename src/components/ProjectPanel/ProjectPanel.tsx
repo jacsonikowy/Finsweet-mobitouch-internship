86a10c1052fddd8aa03ee87d3dc4fc19c406cb6d
jacsonikowy <michal6345@wp.pl>
2023-02-14 21:28:12 +0100

    fix(projects): adding some responsivnes

--- src/components/ProjectPanel/ProjectPanel.tsx
+++ src/components/ProjectPanel/ProjectPanel.tsx
@@ -1,25 +1,41 @@
-import React from 'react'
-import './ProjectPanel.scss'
+import React from "react";
+import "./ProjectPanel.scss";
 
-import TextBlock, { TextblockProps } from '../TextBlock/TextBlock';
-import ButtonArrow from '../ButtonArrow/ButtonArrow';
+import TextBlock, { TextblockProps } from "../TextBlock/TextBlock";
+import ButtonArrow from "../ButtonArrow/ButtonArrow";
 
-interface ProjectPanelProps extends TextblockProps{
-    backgroundActive: boolean,
-    image: string,
-    backgroundWidth?: string,
+interface ProjectPanelProps extends TextblockProps {
+    backgroundActive: boolean;
+    image: string;
+    backgroundWidth?: "max" | "secondary";
 }
 
-const ProjectPanel: React.FC<ProjectPanelProps> = ({backgroundActive, headingText, level, description, image, backgroundWidth}) => {
+const ProjectPanel: React.FC<ProjectPanelProps> = ({
+    backgroundActive,
+    headingText,
+    level,
+    description,
+    image,
+    backgroundWidth,
+}) => {
     return (
-        <div className='projectPanel'>
+        <div className={`projectPanel background1`}>
             <img src={image} alt="" />
-            <div className={`projectPanel-block ${backgroundActive ? "background1" : ""}`}>
-                <TextBlock className="projectPanel-block-textblock" level={level} headingText={headingText} description={description} />
-                <ButtonArrow className="projectPanel-block-buttonArrow" color="#FCD980" text='View project' />
+            <div className={`projectPanel-block secondary`}>
+                <TextBlock
+                    className="projectPanel-block-textblock"
+                    level={level}
+                    headingText={headingText}
+                    description={description}
+                />
+                <ButtonArrow
+                    className="projectPanel-block-buttonArrow"
+                    color="#FCD980"
+                    text="View project"
+                />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default ProjectPanel
+export default ProjectPanel;
--- src/pages/Home/Projects/Projects.tsx
+++ src/pages/Home/Projects/Projects.tsx
@@ -8,15 +8,42 @@ import Church from "../../../assets/images/church.png";
 import TextBlock from "../../../components/TextBlock/TextBlock";
 import ButtonArrow from "../../../components/ButtonArrow/ButtonArrow";
 
-import ProjectPanel from '../../../components/ProjectPanel/ProjectPanel'
+import ProjectPanel from "../../../components/ProjectPanel/ProjectPanel";
 
 const Projects: React.FC = () => {
     return (
         <div className="projects">
-            <ProjectPanel className="item-1" level="h6" headingText="Workhub office Webflow Webflow Design" description="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam " backgroundActive={true} backgroundWidth="396px" image={Workhub}/>
-            <div className="item-2">
-                <ProjectPanel level="h6" headingText="Unisaas Website Design" description="" backgroundActive={true} backgroundWidth="100%" image={Unisaas} />
-                <ProjectPanel level="h6" headingText="Workhub Office Webflow Webflow Design" description="" backgroundActive={false} image={Church}/>
+            <div className="projects-text">
+                <h2>View our projects</h2>
+                <ButtonArrow text="View More" />
+            </div>
+            <div className="grid">
+                <ProjectPanel
+                    className="item-1"
+                    level="h6"
+                    headingText="Workhub office Webflow Webflow Design"
+                    description="Euismod faucibus turpis eu gravida mi. Pellentesque et velit aliquam "
+                    backgroundActive={true}
+                    backgroundWidth="secondary"
+                    image={Workhub}
+                />
+                <div className="item-2">
+                    <ProjectPanel
+                        level="h6"
+                        headingText="Unisaas Website Design"
+                        description=""
+                        backgroundActive={true}
+                        backgroundWidth="max"
+                        image={Unisaas}
+                    />
+                    <ProjectPanel
+                        level="h6"
+                        headingText="Workhub Office Webflow Webflow Design"
+                        description=""
+                        backgroundActive={false}
+                        image={Church}
+                    />
+                </div>
             </div>
         </div>
     );
