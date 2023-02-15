00383e0042e3b40e19e268b50ab4816cb516cc07
jacsonikowy <michal6343@wp.pl>
2023-02-15 15:09:33 +0100

    fix(css): changes in css

--- src/pages/ContactUs/Form/Form.tsx
+++ src/pages/ContactUs/Form/Form.tsx
@@ -9,28 +9,30 @@ const Form: React.FC = () => {
         <div className="contactus-formdiv">
             <TextBlock className="contactus-formdiv-textblock" headingText='Contact Us' level='h2' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.' />
             <form className="contactus-form" action="">
-                <p>
-                    <label className='body1'>Name</label>
-                    <input type="text" placeholder='Enter your name' />
-                </p>
-                <p>
-                    <label>Email</label>
-                    <input type="text" placeholder='Enter your email' />
-                </p>
-                <p>
-                    <label>Subject</label>
-                    <input type="text" placeholder='Provide context'/>
-                </p>
-                <p>
-                    <label>Subject</label>
-                    <input type="text" placeholder='Provide context' />
-                </p>
-                <p className='message'>
-                    <label>Message</label>
-                    <textarea></textarea>
-                </p>
+                <div className="contactus-form-grid">
+                    <p>
+                        <label className='body1'>Name</label>
+                        <input type="text" placeholder='Enter your name' />
+                    </p>
+                    <p>
+                        <label>Email</label>
+                        <input type="text" placeholder='Enter your email' />
+                    </p>
+                    <p>
+                        <label>Subject</label>
+                        <input type="text" placeholder='Provide context'/>
+                    </p>
+                    <p>
+                        <label>Subject</label>
+                        <input type="text" placeholder='Provide context' />
+                    </p>
+                    <p className='message'>
+                        <label>Message</label>
+                        <textarea></textarea>
+                    </p>
+                </div>
+            <Button className="submitBtn" type='submit' text="Send Message" variant='black'/>
             </form>
-            <Button type='submit' text="Send Message" variant='secondary'/>
         </div>
     )
 }
