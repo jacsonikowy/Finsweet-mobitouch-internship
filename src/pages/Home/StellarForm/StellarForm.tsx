1b68bde3abe9a15c4bcd63aab801fdca5c3438d1
jacsonikowy <michal6343@wp.pl>
2023-02-16 14:18:40 +0100

    fix: change of forms

--- src/pages/Home/StellarForm/StellarForm.tsx
+++ src/pages/Home/StellarForm/StellarForm.tsx
@@ -1,26 +1,12 @@
 import React from 'react'
-/* import { useFormik } from 'formik' */
 
 
 import './StellarForm.scss'
 import TextBlock from '../../../components/TextBlock/TextBlock';
-import Button from '../../../components/Button/Button';
 
-import WomanWritingOnPaper from '../../../assets/images/womanwritingonpaper.png'
+import FormStellar from './FormStellar/FormStellar'
 
 const StellarForm: React.FC = () => {
-
-    /* const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            figmaLink: '',
-        },
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        }
-    })
-    */
 
     return (
         <div className='stellarform'>
@@ -30,13 +16,7 @@ const StellarForm: React.FC = () => {
                 </div>
                 <div className="stellarform-flex-second">
                     <TextBlock className='stellarform-flex-second-textblock' level='h5' headingText='Send inquiry' description='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.' />
-                    <form className="form" action="">
-                        <input className="body1" type="text" placeholder='Your Name'/>
-                        <input className="body1" type="text" placeholder='Email'/>
-                        <input className="body1" type="text" placeholder='Paste your Figma design URL'/>
-                        <Button className="submitBtn" text="Submit an Inquiry" variant="primary" />
-                        <Button className="stellarform-flex-second-buttonArrow" text='Get in touch with us' variant='arrowPrimary' withArrow={true} />
-                    </form>
+                    <FormStellar />
                 </div>
             </div>
         </div>
