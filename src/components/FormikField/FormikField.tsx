0f60979c665d72bd37811a21c290e3dbaf19aedf
jacsonikowy <michal6343@wp.pl>
2023-02-16 14:16:00 +0100

    feat: new component FormikField

---
+++ src/components/FormikField/FormikField.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useField } from 'formik'
+
+interface FormikFieldProps {
+    name: string,
+    label?: string,
+    textarea?: boolean,
+    placeholder?: string,
+    id: string,
+    className?: string
+}
+
+const FormikField: React.FC<FormikFieldProps> = ({name, label, textarea, className, ...props}) => {
+    
+    const [field, meta] = useField({name, ...props})
+
+    return (
+        <p className={`${textarea===true ? "message" : null}`}>
+            <label htmlFor={name}>
+                {label}
+            </label>
+            { textarea === false ? (
+            <input className={`form-field ${className} ${meta.error ? "active" : ""}`} {...field} {...props} />
+            ) : (<textarea className={`textarea-field form-field ${meta.error ? "active" : ""}`}></textarea>)}
+            {meta.error ? (<div className='error'>{meta.error}</div>) : null}
+        </p>
+    )
+}
+
+export default FormikField
