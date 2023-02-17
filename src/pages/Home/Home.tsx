7a5a52e11454a6aae271aaf9aee6a7d348adce2e
jacsonikowy <michal6343@wp.pl>
2023-02-17 15:14:36 +0100

    fix: change entire routing, add navbar and footer globally

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,28 @@
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
-import React from 'react';
+import React from "react";
 
-import './Home.scss'
-import Startpage from './Startpage/Startpage';
-import Work from './Work/Work';
-import Projects from './Projects/Projects';
-import Features from './Features/Features';
-import Reviews from './Reviews/Reviews';
-import FAQ from './FAQ/FAQ';
-import StellarForm from './StellarForm/StellarForm';
-import Blog from './Blog/Blog'
+import "./Home.scss";
+import Startpage from "./Startpage/Startpage";
+import Work from "./Work/Work";
+import Projects from "./Projects/Projects";
+import Features from "./Features/Features";
+import Reviews from "./Reviews/Reviews";
+import FAQ from "./FAQ/FAQ";
+import StellarForm from "./StellarForm/StellarForm";
+import Blog from "./Blog/Blog";
 
 const Home: React.FC = () => {
-    return (
-        <>
-            <Navbar />
-            <Startpage />
-            <Work />
-            <Projects />
-            <Features />
-            <Reviews />
-            <FAQ />
-            <StellarForm />
-            <Blog />
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Startpage />
+      <Work />
+      <Projects />
+      <Features />
+      <Reviews />
+      <FAQ />
+      <StellarForm />
+      <Blog />
+    </>
+  );
+};
 
 export default Home;
